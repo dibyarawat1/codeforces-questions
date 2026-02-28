0e0f34c3b7f2c2d4f46caeaab3fc9a6583a67414
dibyarawat1 <dibyarawat1@gmail.com>
2026-02-28 20:15:26 +0530

    commit

---
+++ 800_rating/2044A_easy_problem.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t>0){
+        int n;
+        cin>>n;
+        cout<<n-1<<"\n";
+        t--;
+    }
+}
