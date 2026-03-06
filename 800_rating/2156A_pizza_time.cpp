2bc9c884c4a61478b986c3acfce1383f1ea7f4ff
dibyarawat1 <dibyarawat1@gmail.com>
2026-03-06 18:48:40 +0530

    commit

---
+++ 800_rating/2156A_pizza_time.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void solve(){
+    int n;
+    cin>>n;
+    int ans  = ((n-3)/2)+1;
+  cout<<ans<<endl;
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t>0){
+        solve();
+        t--;
+    }
+}
+/*in this question it was simple ap yaar bus samj mai ana chahiye*/
